a812d0975875c21fbfa4c440eed61d67e6973398
SachsenCoder <SachsenCoder@.(none)>
2015-01-11 18:33:16 +0100

    Cleaned up the source code and removed all useless methods and packages. Because now, the grabbing of the pictures became much more easy, than before! #13

--- Anita.Contracts/Data/LinkNodeInfo.cs
+++ Anita.Contracts/Data/LinkNodeInfo.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using HtmlAgilityPack;
 
 namespace SachsenCoder.Anita.Contracts.Data
 {
@@ -10,7 +9,6 @@ namespace SachsenCoder.Anita.Contracts.Data
     {
         public int CurrentNumber { get; set; }
         public int MaxImageCount { get; set; }
-        public HtmlNode LinkNode { get; set; }
         public Exception Error { get; set; }
     }
 }
--- Anita.Core/Leafs/WikifeetGrabber.cs
+++ Anita.Core/Leafs/WikifeetGrabber.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Net;
 using System.Xml;
 using CsQuery;
-using HtmlAgilityPack;
 using Jurassic;
 using System.Diagnostics;
 using System.IO;
@@ -110,9 +109,6 @@ namespace SachsenCoder.Anita.Core.Leafs
             }
             CQ dom = new CQ(answer, new UTF8Encoding());
             var jEngine = new ScriptEngine();
-            //CQ dom = CQ.CreateFromUrl(celebrityUri.AbsoluteUri);
-            //var htmlDoc = new HtmlDocument();
-            //htmlDoc.Load(answer, new UTF8Encoding());
             answer.Close();
 
             processJavaScript(dom, jEngine);
@@ -146,39 +142,11 @@ namespace SachsenCoder.Anita.Core.Leafs
                 CelebritySearchResult = metaInfos.Data.SearchCelebrityAnswerData,
                 IsFinished = true
             });
-
-            //foreach (var linkNodeInfo in getNextLinkNode(htmlDoc, dom)) {
-            //    if (cancelToken.IsCancellationRequested == true) { return; }
-            //    if (linkNodeInfo.Error != null) {
-            //        OutputError(new ErrorData("FetchCelebrityPictures failed (enumerate linkNodeInfos)", linkNodeInfo.Error));
-            //        continue;
-            //    }
-            //    string picUriPath = string.Empty;
-            //    string thumbUriPath = string.Empty;
-            //    HtmlNode a = linkNodeInfo.LinkNode;
-            //    try {
-            //        picUriPath = a.Attributes["href"].Value;
-            //        thumbUriPath = a.ChildNodes["img"].Attributes["src"].Value;
-            //    }
-            //    catch (NullReferenceException ex) {
-            //        OutputError(new ErrorData(string.Format("Problem with URL! The picture \"{0}\" will be skipped!", picUriPath), ex));
-            //        continue;
-            //    }
-            //    var progressInfo = new FetchProgressInfo
-            //    {
-            //        CelebritySearchResult = metaInfos.Data.SearchCelebrityAnswerData,
-            //        LinkNodeInfo = linkNodeInfo,
-            //        PicUriPath = picUriPath
-            //    };
-            //    OutputFetchProgressInfo(progressInfo);
-            //    fetchImage(localStoreRootPath, celebrityName, picUriPath);
-            //}
-            //OutputFetchProgressInfo(new FetchProgressInfo
-            //{
-            //    CelebritySearchResult = metaInfos.Data.SearchCelebrityAnswerData,
-            //    IsFinished = true
-            //});
         }
+
+        public event Action<UniqueData<IEnumerable<SearchCelebrityAnswerData>>> SearchResult;
+        public event Action<FetchProgressInfo> OutputFetchProgressInfo;
+        public event Action<ErrorData> OutputError;
 
         private void processJavaScript(CQ dom, ScriptEngine jEngine)
         {
@@ -220,115 +188,6 @@ namespace SachsenCoder.Anita.Core.Leafs
             strReader.Dispose();
         }
 
-        public event Action<UniqueData<IEnumerable<SearchCelebrityAnswerData>>> SearchResult;
-        public event Action<FetchProgressInfo> OutputFetchProgressInfo;
-        public event Action<ErrorData> OutputError;
-
-        private IEnumerable<LinkNodeInfo> getNextLinkNode(HtmlDocument htmlDoc, CQ dom)
-        {
-            Exception error = null;
-            var cid = getCid(htmlDoc);
-            var picsLeft = getPicsLeft(htmlDoc);
-            var lastPid = getLastPid(htmlDoc);
-            var linkNodes = htmlDoc.DocumentNode.SelectNodes("//div[@id='thepics']//a");
-            var str = dom.Render();
-            
-            var linkNodes_new = dom.Select("div#conts script");
-            var jEngine = new ScriptEngine();
-            foreach (var node in linkNodes_new) {
-                jEngine.Execute(node.InnerText);
-            }
-            var text = jEngine.GetGlobalValue<string>("messanger.cfname");
-            var maxCount = linkNodes.Count + picsLeft;
-            var currentNumber = 0;
-
-            foreach (var node in linkNodes) {
-                yield return new LinkNodeInfo
-                {
-                    CurrentNumber = ++currentNumber,
-                    MaxImageCount = maxCount,
-                    LinkNode = node
-                };
-            }
-
-            while (picsLeft > 0) {
-                var data = string.Format(@"req=morepics&cid={0}&lastpid={1}", cid, lastPid).ToUriString().ToUTF8Bytes();
-                var webReq = (HttpWebRequest)WebRequest.Create(new Uri(_wikiBaseUri, @"perl/ajax.fpl"));
-                webReq.Method = "POST";
-                webReq.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
-                Stream streamToSend = null;
-                try {
-                    streamToSend = webReq.GetRequestStream();
-                }
-                catch (Exception ex) {
-                    error = ex;
-                }
-                
-                if (error != null) {
-                    yield return new LinkNodeInfo { Error = error };
-                    yield break;
-                }
-                streamToSend.Write(data, 0, data.Length);
-                streamToSend.Close();
-                streamToSend.Dispose();
-
-                HttpWebResponse webResp = null;
-                try {
-                    webResp = (HttpWebResponse)webReq.GetResponse();
-                }
-                catch (Exception ex) {
-                    error = ex;
-                }
-
-                if (error != null) {
-                    yield return new LinkNodeInfo { Error = error };
-                    yield break;
-                }
-                var result = new List<SearchCelebrityAnswerData>();
-                Stream answer = null;
-                try {
-                    answer = webResp.GetResponseStream();
-                }
-                catch (Exception ex) {
-                    error = ex;
-                }
-
-                if (error != null) {
-                    yield return new LinkNodeInfo { Error = error };
-                    yield break;
-                }
-                var sr = new StreamReader(answer, new UTF8Encoding());
-                var answerStrings = sr.ReadToEnd().Split('|');
-                sr.Close();
-                sr.Dispose();
-
-                if (int.TryParse(answerStrings[1], out picsLeft) == false) {
-                    yield break;
-                }
-
-                var lastPidString = answerStrings[2].Split(',').Last();
-                if (int.TryParse(lastPidString, out lastPid) == false) {
-                    yield break;
-                }
-
-                htmlDoc = new HtmlDocument();
-                htmlDoc.LoadHtml(answerStrings[0]);
-                linkNodes = htmlDoc.DocumentNode.SelectNodes("//a");
-
-                foreach (var node in linkNodes) {
-                    yield return new LinkNodeInfo
-                    {
-                        CurrentNumber = ++currentNumber,
-                        MaxImageCount = maxCount,
-                        LinkNode = node
-                    };
-                }
-            }
-
-            yield break;
-        }
-
         private void fetchImage(string localStoreRootPath, string celebrityName, string picUriPath)
         {
             var targetFolder = Path.Combine(localStoreRootPath, celebrityName);
@@ -360,57 +219,6 @@ namespace SachsenCoder.Anita.Core.Leafs
             finally {
                 webClient.Dispose();
             }
-        }
-
-        private static int getCid(HtmlDocument htmlDoc)
-        {
-            var scriptNode = htmlDoc.DocumentNode.SelectNodes("//script[not(@*)]");
-            if (scriptNode == null) {
-                return -1;
-            }
-
-            int cid;
-            var cidString = scriptNode[0].InnerText.Split(';')[0].Split('=')[1].Trim();
-            if (int.TryParse(cidString, out cid) == false) {
-                return -1;
-            }
-            return cid;
-        }
-
-        private static int getPicsLeft(HtmlDocument htmlDoc)
-        {
-            var div = htmlDoc.DocumentNode.SelectSingleNode("//div[@id='morepics']");
-            if (div == null) {
-                return -1;
-            }
-            if (string.IsNullOrWhiteSpace(div.InnerText) == true) {
-                return 0;
-            }
-
-            int picsLeft;
-            var picsLeftString = div.ChildNodes[HtmlNode.HtmlNodeTypeNameText].InnerText.Split(' ')[0];
-            if (int.TryParse(picsLeftString, out picsLeft) == false) {
-                return -1;
-            }
-            return picsLeft;
-        }
-
-        private static int getLastPid(HtmlDocument htmlDoc)
-        {
-            var button = htmlDoc.DocumentNode.SelectSingleNode("//div[@id='morepics']//@onclick");
-            if (button == null) {
-                return -1;
-            }
-
-            int lastPid = 0;
-            var lastPidString = button.Attributes["onclick"].Value;
-            var start = lastPidString.IndexOf('(') + 1;
-            var end = lastPidString.LastIndexOf(')');
-            lastPidString = lastPidString.Substring(start, end - start);
-            if (int.TryParse(lastPidString, out lastPid) == false) {
-                return -1;
-            }
-            return lastPid;
         }
 
         private Uri _wikiBaseUri = new Uri("http://www.wikifeet.com", UriKind.Absolute);
