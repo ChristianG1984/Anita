5c78097a9b1f94a788d86a0202e09e8672db76fd
ChristianG1984 <Christian.G2003@outlook.com>
2019-03-28 19:14:17 +0000

    Just added a simple test case.

---
+++ Anita.Api.Tests/MyTestShould.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anita.Api.Tests
+{
+    [TestFixture]
+    public class MyTestShould
+    {
+        [Test]
+        public void
+        ensure_that_1_plus_1_is_still_2() {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
